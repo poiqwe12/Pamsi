68b622959197ac1a942f9eb8ef4ad2fe411d67f2
lglowack <poiqwe97@gmail.com>
2019-03-17 14:33:45 +0100

    merge sort

--- dane.hh
+++ dane.hh
@@ -95,7 +95,7 @@ Data<Type, Wymiar> Bumble_Sort(Data<Type, Wymiar> D)
         change = false;
         for (unsigned int i = 0; i < Wymiar - shift; ++i)
         {
-            if (D[i+1] < D[i])
+            if (D[i + 1] < D[i])
             {
                 Swap(D[i + 1], D[i]);
                 change = true;
@@ -106,21 +106,85 @@ Data<Type, Wymiar> Bumble_Sort(Data<Type, Wymiar> D)
     return D;
 }
 /*   Insertion Sort      */
-template<typename Type,int Wymiar>
-Data<Type,Wymiar> Insertion_Sort(Data<Type,Wymiar> D)
+template <typename Type, int Wymiar>
+Data<Type, Wymiar> Insertion_Sort(Data<Type, Wymiar> D)
 {
     int j;
-    for(unsigned int i = 1;i<Wymiar;++i)
+    for (unsigned int i = 1; i < Wymiar; ++i)
     {
-        Type r=D[i];
-        j=i-1;
-        while((j>=0)&&(r<D[j]))
+        Type r = D[i];
+        j = i - 1;
+        while ((j >= 0) && (r < D[j]))
         {
-            D[j+1]=D[j];
+            D[j + 1] = D[j];
             --j;
         }
-        D[j+1]=r;
-
+        D[j + 1] = r;
     }
+    return D;
+}
+/*   Merge Sort          */
+template <typename Type, int Wymiar>
+void Merge(Data<Type, Wymiar> &D, Type *copy, int start, int mid, int end)
+{
+    for (int i = start; i <= end; ++i)
+    {
+        copy[i] = D[i];
+    }
+    int var_L=start;
+    int var_R=mid+1;
+    int var_D=start;
+    
+    while((var_L<=mid)&&(var_R<=end))
+    {
+        if(copy[var_L]<copy[var_R])
+        {
+            D[var_D]=copy[var_L];
+            var_L++;
+        }
+        else
+        {
+            D[var_D]=copy[var_R];
+            var_R++;
+        }
+        var_D++;
+    }
+    
+    while(var_L<=mid)
+    {
+        D[var_D]=copy[var_L];
+        var_D++;
+        var_L++;
+    }
+    while(var_R<=end)
+    {
+        D[var_D]=copy[var_R];
+        var_D++;
+        var_R++;
+    }
+
+
+
+
+
+
+}
+template <typename Type, int Wymiar>
+void Merge_Sort2(Data<Type, Wymiar> &D, Type *copy, int start_array, int end_array)
+{
+    if (start_array < end_array)
+    {
+        int q = (start_array + end_array) / 2;
+        Merge_Sort2(D, copy, start_array, q);
+        Merge_Sort2(D, copy, q + 1, end_array);
+        Merge(D, copy, start_array, q, end_array);
+    }
+}
+template <typename Type, int Wymiar>
+Data<Type, Wymiar> Merge_Sort(Data<Type, Wymiar> D)
+{
+    Type *copy = new Type[Wymiar]; //Tablica wymagana do sortowania
+    Merge_Sort2(D, copy, 0, Wymiar - 1);
+    delete(copy);
     return D;
 }
--- main.cpp
+++ main.cpp
@@ -10,11 +10,16 @@ using namespace std;
 #define ROZMIAR 1000000
 int main()
 {
-    Data<int,ROZMIAR> D(0);
-    cout<<D<<endl;
-    //Data<int,ROZMIAR> Sorted_Data=Bumble_Sort(D);
-    //cout<<"Succes?  "<<Sorted_Data.Check_Sort()<< endl;
-    //cout<<Sorted_Data;
+    for(int i = 0;i<3;i++)
+    {
+    Data<int,ROZMIAR> *D=new Data<int,ROZMIAR>(i);
+    //cout<<*D<<endl;
+    //Marge(D,10,100);
+    Data<int,ROZMIAR> *Sorted_Data=new Data<int,ROZMIAR>(0);
+    *Sorted_Data=Merge_Sort(*D);
+    cout<<"Succes?  "<<Sorted_Data->Check_Sort()<< endl;
+   //cout<<*Sorted_Data;
+    }
     //Sorted_Data= Insertion_Sort(D);
     //cout<<"Succes?  "<<Sorted_Data.Check_Sort()<<endl;
     //cout<< Sorted_Data;
